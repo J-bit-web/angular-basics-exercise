880be42a41e4b710068ea543e4ad20c8c254ec43
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-10 00:59:27 -0500

    Some updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './article/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
---
+++ src/app/article/articles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-articles',
+  template: '<app-articles></app-articles><app-articles></app-articles>',
+  
+})
+export class ArticlesComponent {
+
+}
